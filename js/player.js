a4efdc1c4dd17a69269b06e8d107163be98c95f0
Gal Gordon <gal.gordon@gmail.com>
2022-05-25 17:49:01 +0300

    reduced hard coded values by 20%

--- js/player.js
+++ js/player.js
@@ -5,16 +5,16 @@ const createMainContent = () => {
 
         const image = document.createElement('img');
         image.setAttribute('src', './images/phone.svg');
-        image.style.width = '600px'
+        image.style.width = '480px'
         container.appendChild(image);
 
         //Main container
         mainContent = document.createElement('div');
         mainContent.style.position = 'absolute';
         mainContent.style.overflow = 'hidden';
-        mainContent.style.width = '390px';
-        mainContent.style.height = '844px';
-        mainContent.style.top = '75px';
+        mainContent.style.width = '312px';
+        mainContent.style.height = '675.2px';
+        mainContent.style.top = '60px';
 
         container.appendChild(mainContent);
 
